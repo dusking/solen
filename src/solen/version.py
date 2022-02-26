276d84b067dd4561b62ddc629595a3f0610b74be
dusking@gmail.com <dusking@gmail.com>
2022-02-26 06:53:50 +0200

    rebuilt docs

--- src/solen/version.py
+++
@@ -1,3 +0,0 @@
-import pkg_resources
-
-__version__ = pkg_resources.require("solen")[0].version
--- tests/test_sample.py
+++
@@ -1,6 +0,0 @@
-from solen import Solen
-
-
-def test_env():
-    solen = Solen("dev")
-    assert solen.rpc == "https://api.devnet.solana.com"
